5510b018ad46c6bfd2ce3538b0a1ba7d29ea8889
ClintonBeyene <79169975+ClintonBeyene@users.noreply.github.com>
2024-02-09 22:07:26 +0000

    modified get connection from db

--- database.py
+++ database.py
@@ -26,11 +26,19 @@ def get_connection():
   
 def get_connection_from_db(id):
   global conn, cur
+  conn = psycopg2.connect(
+  host = os.environ['HOST_NAME'],
+  dbname = os.environ['DATABASE'],
+  user = os.environ['USERNAME'],
+  password = 'OyQubbAArOP7MUYvAQGAGTNsWCIXvu7U',
+  port = os.environ['PORT_ID']
+)
   cur =conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     
   cur.execute(('SELECT * FROM jobs WHERE id = %s'), (id,))
   
   job = []
-  for row in cur.fetchall():
-    job.append(dict(row))
-  return job
+  if len(job) == 0:
+    return None
+  else:
+    return job
