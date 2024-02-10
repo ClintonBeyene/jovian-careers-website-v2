b58599831a59ccc8ba528060d8a0e2466f81299b
ClintonBeyene <79169975+ClintonBeyene@users.noreply.github.com>
2024-02-10 01:13:08 +0000

    modified

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ from database import get_connection, get_connection_from_db
 
 app = Flask(__name__)
   
-@app.route("/")
+@app.route('/')
 def hello_world():
   jobs = get_connection()
   return render_template('home.html', jobs=jobs)
 
-@app.route("/api/jobs")
+@app.route('/api/jobs')
 def job_lists():
   jobs = get_connection()
   return jsonify(jobs)
 
-@app.route("/job/<id>")
+@app.route('/job/<id>')
 def show_job(id):
   job = get_connection_from_db(id)
-  return jsonify(job)
+
+  if not job:
+    return "Not Found", 404
+  else:
+    return render_template('jobpage.html', job=job)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
